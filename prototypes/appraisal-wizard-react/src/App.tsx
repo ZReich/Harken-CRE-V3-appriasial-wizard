7e044264a5fb1279d7c597c5eae1e71d2eb09e96
monta <monta@local>
2025-12-30 16:44:52 -0700

    Debug: log route changes, wrap Outlet in keyed div

--- prototypes/appraisal-wizard-react/src/App.tsx
+++ prototypes/appraisal-wizard-react/src/App.tsx
@@ -1,14 +1,23 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import { WizardProvider } from './context/WizardContext';
 import { ToastProvider } from './context/ToastContext';
+import { useEffect } from 'react';
 
 function App() {
   const location = useLocation();
   
+  // Debug: log route changes
+  useEffect(() => {
+    console.log('[App] Route changed to:', location.pathname);
+  }, [location.pathname]);
+  
   return (
     <ToastProvider>
       <WizardProvider>
-        <Outlet key={location.pathname} />
+        {/* Force full remount on route change */}
+        <div key={location.pathname}>
+          <Outlet />
+        </div>
       </WizardProvider>
     </ToastProvider>
   );
