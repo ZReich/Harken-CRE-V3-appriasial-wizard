880d537da206845f8cb253cfd57d4496009ff6b4
monta <monta@local>
2025-12-30 16:54:10 -0700

    Revert to BrowserRouter with Routes at root level

--- prototypes/appraisal-wizard-react/src/App.tsx
+++ prototypes/appraisal-wizard-react/src/App.tsx
@@ -1,31 +1,4 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import { WizardProvider } from './context/WizardContext';
-import { ToastProvider } from './context/ToastContext';
-import { useEffect, useState } from 'react';
-
-function App() {
-  const location = useLocation();
-  const [renderKey, setRenderKey] = useState(0);
-  
-  // Debug: log route changes and force re-render
-  useEffect(() => {
-    console.log('[App] Route changed to:', location.pathname);
-    setRenderKey(k => k + 1);
-  }, [location.pathname]);
-  
-  // Also log on every render
-  console.log('[App] Rendering with pathname:', location.pathname, 'key:', renderKey);
-  
-  return (
-    <ToastProvider>
-      <WizardProvider>
-        {/* Force full remount on route change */}
-        <div key={`${location.pathname}-${renderKey}`}>
-          <Outlet />
-        </div>
-      </WizardProvider>
-    </ToastProvider>
-  );
+// App.tsx is no longer used - routes are defined in main.tsx
+export default function App() {
+  return null;
 }
-
-export default App;
--- prototypes/appraisal-wizard-react/src/main.tsx
+++ prototypes/appraisal-wizard-react/src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { WizardProvider } from './context/WizardContext'
+import { ToastProvider } from './context/ToastContext'
 import './index.css'
 
 import TemplatePage from './pages/TemplatePage'
@@ -11,23 +11,21 @@ import SubjectDataPage from './pages/SubjectDataPage'
 import AnalysisPage from './pages/AnalysisPage'
 import ReviewPage from './pages/ReviewPage'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { index: true, element: <Navigate to="/template" replace /> },
-      { path: 'template', element: <TemplatePage /> },
-      { path: 'document-intake', element: <DocumentIntakePage /> },
-      { path: 'setup', element: <SetupPage /> },
-      { path: 'subject-data', element: <SubjectDataPage /> },
-      { path: 'analysis', element: <AnalysisPage /> },
-      { path: 'review', element: <ReviewPage /> },
-    ],
-  },
-])
-
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />,
+  <BrowserRouter>
+    <ToastProvider>
+      <WizardProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/template" replace />} />
+          <Route path="/template" element={<TemplatePage />} />
+          <Route path="/document-intake" element={<DocumentIntakePage />} />
+          <Route path="/setup" element={<SetupPage />} />
+          <Route path="/subject-data" element={<SubjectDataPage />} />
+          <Route path="/analysis" element={<AnalysisPage />} />
+          <Route path="/review" element={<ReviewPage />} />
+        </Routes>
+      </WizardProvider>
+    </ToastProvider>
+  </BrowserRouter>,
 )
 
