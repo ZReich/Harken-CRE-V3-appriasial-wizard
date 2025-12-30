1479672ac1c029e844fdd14a403bcbb0afb3bae7
monta <monta@local>
2025-12-30 16:51:34 -0700

    Add more debug logging for route changes

--- prototypes/appraisal-wizard-react/src/App.tsx
+++ prototypes/appraisal-wizard-react/src/App.tsx
@@ -1,21 +1,26 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import { WizardProvider } from './context/WizardContext';
 import { ToastProvider } from './context/ToastContext';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const location = useLocation();
+  const [renderKey, setRenderKey] = useState(0);
   
-  // Debug: log route changes
+  // Debug: log route changes and force re-render
   useEffect(() => {
     console.log('[App] Route changed to:', location.pathname);
+    setRenderKey(k => k + 1);
   }, [location.pathname]);
+  
+  // Also log on every render
+  console.log('[App] Rendering with pathname:', location.pathname, 'key:', renderKey);
   
   return (
     <ToastProvider>
       <WizardProvider>
         {/* Force full remount on route change */}
-        <div key={location.pathname}>
+        <div key={`${location.pathname}-${renderKey}`}>
           <Outlet />
         </div>
       </WizardProvider>
